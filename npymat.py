a9a8c3d87a68338d7d5ed1f59d9e039f873f1b0e
abhay62-eng <abhichoudhary5t@gmail.com>
2026-01-21 16:41:25 +0530

    Add files via upload

---
+++ npymat.py
@@ -0,0 +1,7 @@
+# we creat a data set using the numpy as follow with matplotlib
+import matplotlib.pyplot as plt
+x=[10,20,30,40,50,60]
+y=[13,14,45,32,43,96]
+plt.title("Bar graph")
+plt.bar(x,y,color='dodgerblue',width=5)
+plt.show()
